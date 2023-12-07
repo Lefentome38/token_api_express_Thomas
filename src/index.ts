f5d8e7bd64c81b0ff1f72c742cfbe29e4582c9f8
aymeric <rambaudaymeric@gmail.com>
2023-12-07 16:12:57 +0100

    middlemares

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { Sequelize} from "sequelize"
 import cors from "cors"
 import bodyParser from "body-parser"
-import express from 'express'
+import express, { NextFunction, Request, Response } from 'express'
 import "dotenv/config"
 
 import { FreeGameModel } from "./model/FreeGame" // la table
@@ -18,34 +18,40 @@ const PORT = process.env.PORT as string;
 app.use(cors())
 app.use(bodyParser.json())
 
-
-
 const sequelize = new Sequelize({
   dialect: "sqlite",
   storage: "./BDD.sqlite",
 })
 
-
-
 export const FreeGame = FreeGameModel(sequelize);
 export const OfficialGames = OfficialGameModel(sequelize)
 export const User = UserModel(sequelize)
+
+sequelize
+// .sync({ force: true })
+.sync()
+.catch(error => {
+console.error('Erreur de synchronisation', error)
+})
+
+export async function checkToken(req: Request, res: Response, next: NextFunction){
+  const authorization = req.headers.authorization
+  console.log('authorization', authorization)
+  if (!authorization) {
+    res.status(401).send("No")
+  }
+  else{
+    next();
+  }
+}
 
 const apiRouter = express.Router();
 apiRouter.use('/free-games',FreeGameRouter )
 apiRouter.use('/official-games', OfficialGamesRouter )
 apiRouter.use('/auth', AuthRouter)
-apiRouter.use('/user', UserRouter)
+apiRouter.use('/users', UserRouter)
 
 app.use('/api', apiRouter)
-
-
-
-sequelize
-.sync({ force: true })
-.catch(error => {
-console.error('Erreur de synchronisation', error)
-})
 
 app.listen( parseInt(PORT), () =>
   console.log("Server is listening on port " + PORT + "...")
--- src/router/Auth.ts
+++ src/router/Auth.ts
@@ -1,3 +1,52 @@
 import { Router } from "express";
+import { User } from "..";
+import "dotenv/config"
+import bcrypt from 'bcrypt';
+import jwt from "jsonwebtoken"
+
+const saltRounds = 10;
 
 export const AuthRouter = Router()
+
+AuthRouter.post('/local/register', async (req,res) => { // pour créer un jeu et verifier si le email est déja utilisé
+    let {email,username, password} = req.body
+    const userWithEmail = await User.findOne({ where : { email: email }})
+    if(userWithEmail){
+      res.status(400).json({ message: "L'email est déjà pris"})
+    }
+    else {
+      const hash = await bcrypt.hash(password, saltRounds);
+      const newUser = await User.create({email, password: hash, username})
+      if(newUser){
+        delete newUser.dataValues.password
+        res.json(newUser)
+      }
+      else {
+        res.status(400).json({ message: "utilisateur non créé" })
+      }
+    }
+  })
+
+// const match = await bcrypt.compare(myPlaintextPassword, hash);
+  
+AuthRouter.post('/local', async (req,res) => { 
+    const {identifier, password} = req.body
+    const user = await User.findOne({ where: { email: identifier }})
+
+    if(user !== null){
+        const match = await bcrypt.compare(req.body.password, user?.dataValues.password);
+        if(match){
+            const token = jwt.sign({ id: user.dataValues.id }, process.env.JWT_SECRET!)
+            res.json({
+                message: "ok",
+                token
+            })
+        }
+        else{
+          res.status(400).json({ message: "non" })
+        }
+    }
+    else {
+        res.status(400).json({ message: "l'utilisateur n'existe pas" })
+    }
+})
